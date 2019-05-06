e188bf9a54b63c5dc2a02db0e383188b0cab436e
FahedSabellioglu <45632631+FahedSabellioglu@users.noreply.github.com>
2019-05-06 22:38:26 +0300

    updated main

--- Main.py
+++ Main.py
@@ -14,21 +14,28 @@ from SQL import *
 import serial
 Window.size = (800, 600)
 
-Functions.load_string("Month")
 
-class Months(BoxLayout):
+
+Functions.load_string("Weeks")
+
+class Weeks(BoxLayout):
     def __init__(self,**kwargs):
-        super(Months,self).__init__(**kwargs)
+        super(Weeks,self).__init__(**kwargs)
 
 
 class UserAttendance(Screen):
     Functions.load_string("UserAttendance")
     def on_pre_enter(self, *args):
+        Functions.existbutton(self)
+        data = list(student_info(Cache.get('user','login'))[0])
+        self.ids['user_name'].text = " ".join(data[0:2])
+        self.ids['user_id'].text = str(data[4])
+
         if Cache.get("counter","value") == None:
             Functions.Calendar(self,2)
         else:
             Functions.Calendar(self,Cache.get("counter","value"))
-    def Exit(self):
+    def Exit(self,dt):
         pages.append(LoginPage(name="LoginPage"))
         Functions.ChangePage(pages,screen)
 
@@ -55,6 +62,10 @@ class LoginPage(Screen):
 
     def on_enter(self, *args):
         Clock.schedule_interval(self.scan,2)
+        Clock.schedule_interval(self.clearwarning,4)
+        if Cache.get('id', 'user') != None:
+            Cache.remove('id', 'user')
+
     def scan(self,dt):
         self.ids['st_id'].text = ''
         self.ids['st_name'].text = ''
@@ -71,6 +82,12 @@ class LoginPage(Screen):
                     self.ids['st_warn'].opacity = 0
                 elif values == 0:
                     self.ids['st_warn'].opacity = 1
+                elif values == -1:
+                    self.ids['st_warn'].text = "Attendance already taken"
+                    self.ids['st_warn'].opacity = 1
+                Cache.remove('id','user')
+    def clearwarning(self,dt):
+        self.ids['st_warn'].opacity = 0
 
     def OnLogin(self):
         pages.append(AttendanceLogin(name="LoginPage"))
@@ -79,9 +96,11 @@ class LoginPage(Screen):
     def LoginCre(self):
         if Functions.AttendanceLogin(self) and self.ids['CheckLogin'].active==True:
             pages.append(Admin_Page(name="Admin_Page"))
+            Clock.unschedule(self.scan)
             Functions.ChangePage(pages, screen)
         elif Functions.AttendanceLogin(self) and self.ids['CheckLogin'].active==False:
             pages.append(UserAttendance(name='User_page'))
+            Clock.unschedule(self.scan)
             Cache.append("user", "login", self.ids["input_id"].text)
             Cache.append('counter','value',1)
             Functions.ChangePage(pages,screen)
@@ -106,16 +125,27 @@ class LoginPage(Screen):
 
 class Admin_Page(Screen):
     Functions.load_string("Admin_Page")
+
+
     def on_pre_enter(self, *args):
         Functions.on_pre_enter(self)
+        data = list(get_ins_info(Cache.get('instructor','email'))[0])
+        self.ids['ins_name'].text = " ".join(data[0:2])
+        # Functions.existbutton(self)
+
+    def Exit(self):
+        pages.append(LoginPage(name='LoginPage'))
+        Functions.ChangePage(pages, screen)
+
     def profile(self):
         Functions.profile(self)
-    def del_students(self):
+    def del_students(self,dt):
         Functions.on_delete(self)
 
 class AttendanceApp(App):
     def build(self):
         inspector.create_inspector(Window,self)
+        self.icon = r"C:\Users\fahedshaabani\Documents\GitHub\Attendance\data\img\3592659-128.png"
         try:
             self.arduino = serial.Serial(
             port='COM3',
@@ -132,12 +162,12 @@ class AttendanceApp(App):
         self.new.flush()
         self.new.flushInput()
         self.new.flushOutput()
-        print line.split()
+        print line.split(),'arduino'
         if len(line.split()) != 0 and "FF" not in line:
             if Cache.get('id', 'user') == None:
                 Cache.append('id', 'user', line.strip())
             else:
-                Cache.remove('id', 'user')
+                Cache.remove('id', 'user'),'purge'
                 Cache.append('id', 'user', line.strip())
 
 
