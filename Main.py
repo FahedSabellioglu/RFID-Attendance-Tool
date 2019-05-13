fba3727f6ce8f8f510f5140d2acb98f7ebc64762
FahedSabellioglu <45632631+FahedSabellioglu@users.noreply.github.com>
2019-05-13 11:44:41 +0300

    New interface and background rfid scan

--- Main.py
+++ Main.py
@@ -7,6 +7,11 @@ from kivy.lang import Builder
 from kivy.cache import Cache
 from kivy.core.window import Window
 from kivy.modules import inspector
+from Arduino import *
+from kivy.clock import Clock
+from functools import partial
+from SQL import *
+import serial
 Window.size = (800, 600)
 
 Functions.load_string("Month")
@@ -29,12 +34,12 @@ class UserAttendance(Screen):
 
     @staticmethod
     def callback(self):
-        Month = list(calendar.month_abbr).index(self.text)
+        weekNumber = int(self.text.split()[-1])
         if Cache.get("counter",'value') == None:
-            Cache.append("counter", 'value', Month)
+            Cache.append("counter", 'value', weekNumber)
         else:
             Cache.remove("counter","value")
-            Cache.append("counter",'value',Month)
+            Cache.append("counter",'value',weekNumber)
         pages.append(UserAttendance(name="UserAttendance"))
         Functions.ChangePage(pages,screen)
 
@@ -43,71 +48,110 @@ class UserAttendance(Screen):
         Functions.NotePopUp(self,self.text)
 
 
-class LoginPage(Screen):
 
+
+class LoginPage(Screen):
     Functions.load_string("LoginPage")
+
+    def on_enter(self, *args):
+        Clock.schedule_interval(self.scan,2)
+    def scan(self,dt):
+        self.ids['st_id'].text = ''
+        self.ids['st_name'].text = ''
+        if Cache.get('id','user') != None:
+            value = card_exist(Cache.get('id','user'))
+            if len(value) == 0:
+                Clock.unschedule(self.scan)
+                Functions.new_card_pop(self)
+            else:
+                values = attendance(Cache.get('id','user'))
+                if values == 1:
+                    self.ids['st_id'].text = str(value[0][3])
+                    self.ids['st_name'].text = value[0][0] + " "+ value[0][1]
+                    self.ids['st_warn'].opacity = 0
+                elif values == 0:
+                    self.ids['st_warn'].opacity = 1
+
     def OnLogin(self):
         pages.append(AttendanceLogin(name="LoginPage"))
         Functions.ChangePage(pages,screen)
 
+    def LoginCre(self):
+        if Functions.AttendanceLogin(self) and self.ids['CheckLogin'].active==True:
+            pages.append(Admin_Page(name="Admin_Page"))
+            Functions.ChangePage(pages, screen)
+        elif Functions.AttendanceLogin(self) and self.ids['CheckLogin'].active==False:
+            pages.append(UserAttendance(name='User_page'))
+            Cache.append("user", "login", self.ids["input_id"].text)
+            Cache.append('counter','value',1)
+            Functions.ChangePage(pages,screen)
+
+    def CheckButtonControl(self):
+        if self.ids["CheckLogin"].active == True:
+            self.ids['warning'].opacity = 0
+            self.ids["input_pass"].opacity = 1
+            self.ids["input_pass"].disabled = False
+            self.ids["login_passw"].opacity = 1
+            self.ids["input_id"].pos_hint = {"center_x": .65, "center_y": .520}
+            self.ids['login_id'].pos_hint = {"center_x": .40, "center_y": .520}
+            self.ids["input_pass"].pos_hint = {"center_x": .65, "center_y": .370}
+        else:
+            self.ids['warning'].opacity = 0
+            self.ids["input_pass"].opacity = 0
+            self.ids["login_passw"].opacity = 0
+            self.ids["input_pass"].disabled = True
+            self.ids["login_id"].pos_hint = {"center_x": .40, "center_y": .420}
+            self.ids["input_id"].pos_hint = {"center_x": .65, "center_y": .420}
+
 
 class Admin_Page(Screen):
     Functions.load_string("Admin_Page")
-    def on_enter(self, *args):
+    def on_pre_enter(self, *args):
         Functions.on_pre_enter(self)
-
-
-
-class AttendanceLogin(Screen):
-
-    Functions.load_string("AttendanceLogin")
-
-    def checkButton(self):
-        if self.ids["CheckLogin"].active == True:
-            self.ids["input_password"].opacity = 1
-            self.ids["input_password"].disabled = False
-            self.ids["txt_password"].opacity = 1
-            self.ids["input_userid"].pos_hint = {"center_x":.58,"center_y":.66}
-            self.ids["txt_userid"].pos_hint = {"center_x":.35,"center_y":.66}
-        else:
-            self.ids["input_password"].opacity = 0
-            self.ids["txt_password"].opacity = 0
-            self.ids["input_password"].disabled = True
-            self.ids["txt_userid"].pos_hint = {"center_x":.35,"center_y":.56}
-            self.ids["input_userid"].pos_hint = {"center_x":.58,"center_y":.56}
-        inspector.create_inspector(Window, self.ids["input_userid"])
-
-    def Login(self):
-        if Functions.AttendanceLogin(self) and (self.ids["CheckLogin"].active == False):
-            pages.append(UserAttendance(name="UserAttendance"))
-            Cache.append("User","username",self.ids["input_userid"].text)
-            Functions.ChangePage(pages,screen)
-        elif Functions.AttendanceLogin(self):
-            pages.append(Admin_Page(name="Admin_Page"))
-            Cache.append("User","username",self.ids["input_userid"].text)
-            Functions.ChangePage(pages,screen)
-
-    def Back(self):
-        try:
-            screen.switch_to(pages[0])
-        except:
-            screen.current = pages[0].name
-
-
+    def profile(self):
+        Functions.profile(self)
+    def del_students(self):
+        Functions.on_delete(self)
 
 class AttendanceApp(App):
     def build(self):
+        inspector.create_inspector(Window,self)
+        try:
+            self.arduino = serial.Serial(
+            port='COM3',
+            baudrate=9600)
+        except:
+            pass
+
+        Clock.schedule_interval(self.update,5)
         screen.current = "LoginPage"
         return screen
+    def update(self,*args):
+        self.new = self.arduino
+        line = self.new.readline(self.new.inWaiting())
+        self.new.flush()
+        self.new.flushInput()
+        self.new.flushOutput()
+        print line.split()
+        if len(line.split()) != 0 and "FF" not in line:
+            if Cache.get('id', 'user') == None:
+                Cache.append('id', 'user', line.strip())
+            else:
+                Cache.remove('id', 'user')
+                Cache.append('id', 'user', line.strip())
 
 
 pages = [LoginPage(name="LoginPage"),
          ]
 Cache.register('counter',limit=1)
 Cache.register("User",limit = 1)
+Cache.register('user',limit=1)
+Cache.register('days',limit=1)
+Cache.register('id',limit=2)
+Cache.register('instructor',limit=1)
+
 screen = ScreenManager(transition=FadeTransition())
 screen.add_widget(pages[0])
-
 
 AttendanceApp().run()
 
